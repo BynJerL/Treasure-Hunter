4972d36f4258dfb962064d8933d0abd7a8fb91cc
BynJerL <yggcphanq@gmail.com>
2025-08-13 16:45:00 +0700

    Add game.py and mission

---
+++ game.py
@@ -0,0 +1,7 @@
+from adventurer import Adventurer
+
+mission = [
+    {"coor": (5, 5), "finding": "Shard"},
+    {"coor": (10, -2), "finding": "Compass"},
+    {"coor": (-7, 4), "finding": "Animal Bones"}
+]

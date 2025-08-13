25316be4fe32607658d16ed94bcc6a464c0589f9
BynJerL <yggcphanq@gmail.com>
2025-08-13 16:40:02 +0700

    Add inventory, printPos, and getPos

--- adventurer.py
+++ adventurer.py
@@ -1,10 +1,17 @@
 class Adventurer:
-    def __init__(self, name: str, speed: int, initX: int = 0, initY: int = 0):
+    def __init__(self, name: str, speed: int, initX: int = 0, initY: int = 0, inventory: list = None):
         self.name = name
         self.speed = speed
         self.posX = initX
         self.posY = initY
+        self.inventory = inventory or []
     
+    def printPos(self):
+        print(f"{self.name}: (x={self.posX}, y={self.posY})")
+    
+    def getPos(self):
+        return (self.posX, self.posY)
+
     def goToPos(self, x: int, y: int):
         self.posX = x
         self.posY = y

095b5d489c6a05d0468c4c435417b6c6fce295bc
BynJerL <yggcphanq@gmail.com>
2025-08-15 12:34:23 +0700

    Add check map.

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ from adventurer import Adventurer
 missions = [
     {"coor": (5, 5), "finding": "Shard", "found": False},
     {"coor": (10, -2), "finding": "Compass", "found": False},
+    {"coor": (10, -2), "finding": "Ancient Map", "found": False},
     {"coor": (-7, 4), "finding": "Animal Bones", "found": False},
     {"coor": (3, 8), "finding": "Silver Key", "found": False},
     {"coor": (-9, -9), "finding": "Golden Idol", "found": False, "requirement": "Silver Key"}
@@ -36,6 +37,18 @@ def excavate():
     if not foundSomething:
         print(f"You got nothing.")
 
+def check_map():
+    ax, ay = adventurer.getPos()
+    for y in range(ay - 4, ay + 5):
+        for x in range(ax - 4, ax + 5):
+            if (x, y) in [mission.get("coor") for mission in missions if not mission.get("found")]:
+                print("@", end=" ")
+            elif (x, y) in [mission.get("coor") for mission in missions if mission.get("found")]:
+                print("v", end=" ")
+            else:
+                print("-", end=" ")
+        print()
+
 def check_mission():
     return all(mission.get("found") for mission in missions)
 
@@ -57,6 +70,8 @@ while isRunning:
             excavate()
         case "h":
             adventurer.showInventory()
+        case "t":
+            check_map()
         case "p":
             isRunning = False
     
